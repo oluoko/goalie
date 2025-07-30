da8b8b9c15483759691a99fb41d31529fa37e334
oluoko <brianotieno586@gmail.com>
2025-07-30 20:54:14 +0300

    Goalie: Updated Routes

---
+++ backend/controller/goalController.js
@@ -0,0 +1,38 @@
+// @desc       Get goals
+// @route      GET /api/goals
+// @access     Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "goalController: Getting goals" });
+};
+
+// @desc       Create goal
+// @route      POST /api/goals
+// @access     Private
+const createGoal = (req, res) => {
+  res.status(201).json({ message: "goalController: Goal created" });
+};
+
+// @desc       Update goal
+// @route      PUT /api/goals/:id
+// @access     Private
+const updateGoal = (req, res) => {
+  res
+    .status(200)
+    .json({ message: `goalController: Goal with ID ${req.params.id} updated` });
+};
+
+// @desc       Delete goal
+// @route      DELETE /api/goals/:id
+// @access     Private
+const deleteGoal = (req, res) => {
+  res
+    .status(200)
+    .json({ message: `goalController: Goal with ID ${req.params.id} deleted` });
+};
+
+module.exports = {
+  getGoals,
+  createGoal,
+  updateGoal,
+  deleteGoal,
+};
