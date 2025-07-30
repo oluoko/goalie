68133ce9762f325c6e45b6d275b4139cf6617b8e
oluoko <brianotieno586@gmail.com>
2025-07-30 21:18:09 +0300

    Goalie: Error Handler

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -9,6 +9,10 @@ const getGoals = (req, res) => {
 // @route      POST /api/goals
 // @access     Private
 const createGoal = (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
   res.status(201).json({ message: "goalController: Goal created" });
 };
 
---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,12 @@
+const errorHandler = (err, req, res, next) => {
+  const statusCoce = res.statusCode ? res.statusCode : 500;
+
+  res.status(statusCoce);
+
+  res.json({
+    "Error Message": err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+module.exports = { errorHandler };
