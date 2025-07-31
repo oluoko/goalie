96693753db96a7b2fb5233d218040129d1e20b2d
oluoko <brianotieno586@gmail.com>
2025-07-31 16:25:26 +0300

    Goalie: Auth UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,75 @@
+import { useState } from "react";
 import Meta from "../components/Meta";
+import { FaSignInAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted", formData);
+  };
+
   return (
-    <div>
-      <Meta title="Log In" />
-    </div>
+    <>
+      <Meta title="Login" />
+      <section className="heading">
+        <h1>
+          <FaSignInAlt />
+          Login
+        </h1>
+        <p>Welcome. Login and start setting goals</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              placeholder="Enter Your Email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              placeholder="Enter Your Password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Login
+            </button>
+          </div>
+          <p>
+            Don't have an account? <Link to="/register">Register</Link>
+          </p>
+        </form>
+      </section>
+    </>
   );
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,110 @@
+import { useState } from "react";
 import Meta from "../components/Meta";
+import { FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Register() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const { firstName, lastName, email, password, confirmPassword } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted", formData);
+  };
+
   return (
-    <div>
+    <>
       <Meta title="Register" />
-    </div>
+      <section className="heading">
+        <h1>
+          <FaUser />
+          Register
+        </h1>
+        <p>Please create an account to continue</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              name="firstName"
+              id="firstName"
+              placeholder="Enter Your First Name"
+              value={firstName}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="lastName"
+              name="lastName"
+              placeholder="Enter Your Last Name"
+              value={lastName}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              placeholder="Enter Your Email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              placeholder="Enter Your Password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Confirm Your Password"
+              value={confirmPassword}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Register
+            </button>
+          </div>
+          <p>
+            Already have an account? <Link to="/login">Login</Link>
+          </p>
+        </form>
+      </section>
+    </>
   );
 }
 
